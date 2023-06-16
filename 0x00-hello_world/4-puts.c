5b8bb98b8113a6a7473c90bffa5cbe33fd2a772a
Christian Nwachukwu <programmerchris6002@gmail.com>
2023-06-15 21:28:12 -0700

    Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/*
+ * main - This prints Programming is like building a multilingual puzzle
+ * Returns: This will Return 0
+ */
+
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle\n");
+	return (0);
+}
