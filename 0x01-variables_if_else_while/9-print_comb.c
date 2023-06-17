81a34f822986a8645009f3df532338e1ef7243e1
Christian Nwachukwu <programmerchris6002@gmail.com>
2023-06-17 15:52:29 -0700

    Patience, persistence and perspiration make an unbeatable combination for success

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - This Prints single digit numbers
+ * Return: This Returns 0
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; ++i)
+	{
+		putchar(i + '0');
+
+		if (i < 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+
+	return (0);
+}
