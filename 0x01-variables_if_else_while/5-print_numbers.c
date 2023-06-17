4a280ec7fcb9e7769148c522772c344ca953c5da
Christian Nwachukwu <programmerchris6002@gmail.com>
2023-06-16 21:51:37 -0700

    Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - This prints 0 to 9
+ * Return: This Returns 0
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; ++i)
+		printf("%d", i);
+
+	return (0);
+}
