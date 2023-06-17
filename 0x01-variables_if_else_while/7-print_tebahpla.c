10c0cc02215961c7a0d349245bb6ad0e8b45e23e
Christian Nwachukwu <programmerchris6002@gmail.com>
2023-06-17 15:12:15 -0700

    Smile in the mirror

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - This Prints alphabets in reverse order
+ * Return: This Returns
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 122; i >= 97; ++i)
+		putchar(i);
+
+	putchar('\n');
+
+	return (0);
+}
