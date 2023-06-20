2a3ca2872c02cebeba75bcf1b3cf9211ff0e2e5d
Christian Nwachukwu <programmerchris6002@gmail.com>
2023-06-20 16:32:49 -0700

    98 Battery Street, the OG

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -9,11 +9,12 @@
 void print_to_98(int n)
 {
 	int i;
+
 	if (n < 98)
-		for (i = n; i < n; i++)
+		for (i = n; i < 98; i++)
 			printf("%d, ", i);
 	else if (n > 98)
-		for (i = n; i > n; i--)
+		for (i = n; i > 98; i--)
 			printf("%d, ", i);
 	printf("98");
 	printf("\n");
