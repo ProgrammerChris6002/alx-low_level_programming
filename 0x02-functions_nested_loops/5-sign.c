bedd67bb973a050eaad1c76ea3b9be7ed35b20a2
Christian Nwachukwu <programmerchris6002@gmail.com>
2023-06-20 13:10:14 -0700

    Sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -10,14 +10,17 @@ int print_sign(int n)
 {
 	if (n > 0)
 	{
-		return ('+1');
+		return (1);
+		putchar('+');
 	}
 	else if (n == 0)
 	{
-		return ('00');
+		return (0);
+		putchar('0');
 	}
 	else if (n < 0){
-		return ('-1');
+		return (-1);
+		putchar('-');
 	}
 
 	return (0);
