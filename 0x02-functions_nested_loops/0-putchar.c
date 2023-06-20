13822055c51103a23984d7a042fa494bedaa45c1
Christian Nwachukwu <programmerchris6002@gmail.com>
2023-06-20 07:01:32 -0700

    Adding a readme

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+  * main - This prints _putchar
+  * Return: This Returns 0
+  */
+
+int main(void)
+{
+
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+README File for 0x02 C Programming Functions tasks in ALX
