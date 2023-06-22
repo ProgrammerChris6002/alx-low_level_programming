519e5f931e9c476b16978bce6b01e1551be1628f
Christian Nwachukwu <programmerchris6002@gmail.com>
2023-06-22 06:54:04 -0700

    The numbers speak for themselves

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+  * print_numbers - This prints digits from 0 to 9
+  * Return: Returns 0
+  */
+
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+		putchar(i);
+	putchar('\n');
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -10,4 +10,7 @@ int _isdigit(int c);
 /* Task 2 */
 int mul(int a, int b);
 
+/* Task 3 */
+void print_numbers(void);
+
 #endif /* MAIN_H */
