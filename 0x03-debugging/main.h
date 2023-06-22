202164dead2e02270974c17f214b9029263e9695
Christian Nwachukwu <programmerchris6002@gmail.com>
2023-06-22 04:53:16 -0700

    Leap year

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -8,4 +8,7 @@ void positive_or_negative(int i);
 /* Task 2 */
 int largest_number(int a, int b, int c);
 
+/* Task 3 */
+void print_remaining_days(int month, int day, int year);
+
 #endif /* MAIN_H */
