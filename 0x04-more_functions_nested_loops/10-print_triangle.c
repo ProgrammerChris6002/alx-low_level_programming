1000c82878ea03b446f4033b97600e954fa181b7
Christian Nwachukwu <programmerchris6002@gmail.com>
2023-06-22 14:59:33 -0700

    I feel like I am diagonally parked in a parallel universe

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -2,21 +2,24 @@
 
 /**
   * print_triangle - This prints a triangle of size n
-  * @n: Size of triangle
+  * @size: Size of triangle
   */
 
 void print_triangle(int size)
 {
 	int i, j, k;
 
-	for (i = 1; i <= size; i++)
-	{
-		for (j = 1; j <= size - i; j++)
-			putchar(' ');
+	if (n > 0)
+		for (i = 1; i <= size; i++)
+		{
+			for (j = 1; j <= size - i; j++)
+				putchar(' ');
 
-		for (k = 1; k <= i; k++)
-			putchar('#');
+			for (k = 1; k <= i; k++)
+				putchar('#');
 
+			putchar('\n');
+		}
+	else
 		putchar('\n');
-	}
 }
