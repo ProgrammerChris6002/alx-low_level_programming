45322cde8b566e2a3b9b2285dd0c0d2c916082c0
Christian Nwachukwu <programmerchris6002@gmail.com>
2023-06-23 04:22:19 -0700

    Learn your times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -14,7 +14,7 @@ void times_table(void)
 			product = i * j;
 			if (j == 0)
 			{
-				_putchar(k + '0');
+				_putchar(product + '0');
 			}
 
 			if (product < 10 && j != 0)
@@ -22,14 +22,14 @@ void times_table(void)
 				_putchar(',');
 				_putchar(' ');
 				_putchar(' ');
-				_putchar(k + '0');
+				_putchar(product + '0');
 			}
-			else if (k >= 10)
+			else if (product >= 10)
 			{
 				_putchar(',');
 				_putchar(' ');
-				_putchar((k / 10) + '0');
-				_putchar((k % 10) + '0');
+				_putchar((product / 10) + '0');
+				_putchar((product % 10) + '0');
 			}
 		}
 		_putchar('\n');
