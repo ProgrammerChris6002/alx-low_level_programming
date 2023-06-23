f7d77a73dc2d4cdd851c280cbc2ad04f729e2adc
Christian Nwachukwu <programmerchris6002@gmail.com>
2023-06-22 17:35:06 -0700

    Sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -10,17 +10,14 @@ int print_sign(int n)
 {
 	if (n > 0)
 	{
-		printf("+");
-		return (1);
+		printf("+1");
 	}
 	else if (n == 0)
 	{
-		printf("0");
-		return (0);
+		printf("00");
 	}
 	else
 	{
-		printf("-");
-		return (-1);
+		printf("-1");
 	}
 }
