29c8509d0f39e9328325c61a3b19d9028dbaecfa
Christian Nwachukwu <programmerchris6002@gmail.com>
2023-06-22 17:07:14 -0700

    In computer class, the first assignment was to write a program to print the first 100 Fibonacci numbers. Instead, I wrote a program that would steal passwords of students. My teacher gave me an A

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#define LARGEST 10000000000
+
 /**
   * main - This prints the first 50 numbers of the Fibonacci Series starting
   * from 1 and 2
@@ -7,18 +9,32 @@
 
 int main(void)
 {
-	int i, n1 = 1, n2 = 2;
-	unsigned long next;
+	unsigned long int n1 = 1, n2 = 2, cover1 = 0, cover2 = 0;
+	unsigned long int hold1, hold2, hold3;
+	int i;
 
-	printf("%d, %d, ", n1, n2);
+	printf("%lu, %lu, ", n1, n2);
 	for (i = 2; i < 50; i++)
 	{
-		next = n1 + n2;
-		printf("%lu", next);
+		if (n1 + n2 > LARGEST || cover2 > 0 || cover1 > 0)
+		{
+			hold1 = (n1 + n2) / LARGEST;
+			hold2 = (n1 + n2) % LARGEST;
+			hold3 = n1 + n2 + hold1;
+			n1 = n2, n2 = hold3;
+			n1 = n2, n2 = hold2;
+			printf("%lu%010lu", cover2, n2);
+		}
+		else
+		{
+			hold2 = n1 + n2;
+			n1 = n2, n2 = hold2;
+			printf("%lu", n2);
+		}
 		if (i != 50)
+		{
 			printf(", ");
-		n1 = n2;
-		n2 = next;
+		}
 	}
 	printf("\n");
 
