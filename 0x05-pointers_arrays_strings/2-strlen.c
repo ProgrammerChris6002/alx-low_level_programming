5f9fd3f706aabfbbea577aed2e1f8404f3bd3ec8
Christian Nwachukwu <programmerchris6002@gmail.com>
2023-06-26 17:50:54 -0700

    This report, by its very length, defends itself against the risk of being read

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+  * _strlen - prints the length of a string
+  * @s: string to be checked
+  * Return: 0
+  */
+
+int _strlen(char *s)
+{
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -5,11 +5,14 @@
 
 int _putchar(char c);
 
-/* Question 1 */
+/* Question 0 */
 void reset_to_98(int *n);
 
 
-/* Question 2 */
+/* Question 1 */
 void swap_int(int *a, int *b);
+
+/* Question 2 */
+int _strlen(char *s);
 
 #endif /* MAIN_H */
