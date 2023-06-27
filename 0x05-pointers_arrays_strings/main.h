73b8384dd9e9a5bdeb22489755927a0cb06a9526
Christian Nwachukwu <programmerchris6002@gmail.com>
2023-06-26 18:03:54 -0700

    Adding all files

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -15,4 +15,30 @@ void swap_int(int *a, int *b);
 /* Question 2 */
 int _strlen(char *s);
 
+/* Question 3 */
+void _puts(char *str);
+
+
+/* Question 4 */
+void print_rev(char *s);
+
+/* Question 5 */
+void rev_string(char *s);
+
+/* Question 6 */
+void puts2(char *str);
+
+/* Question 7 */
+void puts_half(char *str);
+
+/* Question 8 */
+void print_array(int *a, int n);
+
+/* Question 9 */
+char *_strcpy(char *dest, char *src);
+
+/* Question 10 */
+int _atoi(char *s);
+
+
 #endif /* MAIN_H */
