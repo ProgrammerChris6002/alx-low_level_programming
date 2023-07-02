9bd0ce7717894061ec710096342445219a567f54
Christian Nwachukwu <programmerchris6002@gmail.com>
2023-07-02 15:03:02 -0700

    Numbers have life; they're not just symbols on paper

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+  * print_number - Prints an integer
+  * @n: Integer to be printed
+  */
+
+void print_number(int n)
+{
+	if (n < 0)
+	{
+		_putchar('-');
+		n = -n;
+	}
+
+	if (n / 10 != 0)
+		print_number(n / 10);
+	_putchar('0' + n % 10);
+}
