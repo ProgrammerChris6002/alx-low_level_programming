d25b0b59df5ac7eb57eb633e0937d58fc59d46ef
Christian Nwachukwu <programmerchris6002@gmail.com>
2023-07-02 09:57:04 -0700

    strcmp

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -9,17 +9,14 @@
 
 int _strcmp(char *s1, char *s2)
 {
-	int len1;
-	int len2;
+	int len;
 
-	while (s1[len1] != '\0')
-		len1++;
-	while (s2[len2] != '\0')
-		len2++;
-
-	if (len1 > len2)
-		return (15);
-	else if (len1 < len2)
-		return (-15);
+	len = 0;
+	while (s1[len] != '\0' && s2[len] != '\0')
+	{
+		if (s1[len] != s2[len])
+			return (s1[len] - s2[len]);
+		i++;
+	}
 	return (0);
 }
