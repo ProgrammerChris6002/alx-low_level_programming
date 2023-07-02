b260325303f92af0eefe59df122b765f4e53b7bd
Christian Nwachukwu <programmerchris6002@gmail.com>
2023-07-02 07:35:35 -0700

    strcat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -9,13 +9,16 @@
 
 char *_strcat(char *dest, char *src)
 {
-	int len1 = 0, len2 = 0;
+	int len1;
+	int len2;
 
+	len1 = 0;
 	while (*(dest + len1) != '\0')
 	{
 		len1++;
 	}
 
+	len2 = 0;
 	while (*(src + len2) != '\0')
 	{
 		*(dest + len1) = *(src + len2);
