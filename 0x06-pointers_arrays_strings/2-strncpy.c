cd17b5cef80a2aa1b45afe858ba08506bdbd2a31
Christian Nwachukwu <programmerchris6002@gmail.com>
2023-07-02 09:27:18 -0700

    strncpy

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -12,7 +12,7 @@ char *_strncpy(char *dest, char *src, int n)
 {
 	int len = 0;
 
-	while (len < n &&*(src + len) != '\0')
+	while (len < n && *(src + len) != '\0')
 	{
 		*(dest + len) = *(src + len);
 		len++;
