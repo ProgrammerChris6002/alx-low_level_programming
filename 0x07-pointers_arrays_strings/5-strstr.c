388d2ab1c3f489c4a952ff87da5fb681f67d824e
Christian Nwachukwu <programmerchris6002@gmail.com>
2023-07-04 03:55:26 -0700

    strpbrk

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+  * _strstr - locates a substring in a string
+  * @haystack: search location
+  * @needle: substring checked for
+  * Return: pointer to substring beginning in haystack if found, else NULL
+  */
+
+char *_strstr(char *haystack, char *needle)
+{
+
+}
