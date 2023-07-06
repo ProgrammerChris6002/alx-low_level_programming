55bccbb201821d9c75bddd264e7f2eaaab43d625
Christian Nwachukwu <programmerchris6002@gmail.com>
2023-07-06 14:38:16 -0700

    They say we only use a fraction of our brain's true potential. Now that's when we're awake. When we're asleep, we can do almost anything

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -15,6 +15,24 @@ int _strlen(char *s)
 }
 
 /**
+  * _check_palindrome - Checks if a string is a palindrome recursively
+  * @s: string to check
+  * @start: The starting index
+  * @end: the ending string
+  * Return: 1 if the string is a palindrome, 0 otherwise
+  */
+
+int _check_palindrome(char *s, int start, int end)
+{
+	if (start >= end)
+		return (1);
+	else if (s[start] != s[end])
+		return (0);
+	else
+		return (_check_palindrome(s, start + 1, end - 1));
+}
+
+/**
   * is_palindrome - checks if a string is a palindrome or nor
   * @s: string parameter
   * Return: returns 1 if a string is a palindrome and 0 if not
@@ -24,11 +42,5 @@ int is_palindrome(char *s)
 {
 	int length = _strlen(s);
 
-	if (length <= 1)
-		return (1);
-	else if (*s != *(s + length - 1))
-		return (0);
-	else
-		return (is_palindrome(s + 1));
+	return (_check_palindrome(s, 0, length - 1));
 }
-
