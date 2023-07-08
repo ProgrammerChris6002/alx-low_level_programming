209c3f4a213c4fabe16489ee2b953a5e06794c3e
Christian Nwachukwu <programmerchris6002@gmail.com>
2023-07-07 21:39:10 -0700

    A library is not a luxury but one of the necessities of life

--- 0x09-static_libraries/main.c
+++ 0x09-static_libraries/main.c
@@ -1,7 +1,13 @@
 #include "main.h"
 
+/**
+  * main - prints  a string using _puts
+  * Return: 0
+  */
+
 int main(void)
 {
-	_puts("\"At the end of the day, my goal was to be the best hacker\"\n\t- Kevin Mitnick");
+	_puts("\"At the end of the day, my goal was to be the best \
+hacker\"\n\t- Kevin Mitnick");
 	return (0);
 }
