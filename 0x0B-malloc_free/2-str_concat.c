25fee313b3a25200b93738207976ec38a1853409
Christian Nwachukwu <programmerchris6002@gmail.com>
2023-07-11 14:48:02 -0700

    He who is not courageous enough to take risks will accomplish nothing in life

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -32,8 +32,9 @@ char *str_concat(char *s1, char *s2)
 
 	for (i = 0; i < len1; i++)
 		array[i] = s1[i];
+
 	for (j = 0; j < len2; j++)
-		array[i + j] = s1[j];
+		array[i + j] = s2[j];
 
 	array[i + j] = '\0';
 
