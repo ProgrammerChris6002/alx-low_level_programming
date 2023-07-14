f21997c422928b39001b8b2350de2ce8b1f0b578
Christian Nwachukwu <programmerchris6002@gmail.com>
2023-07-13 21:57:52 -0700

    We must accept finite disappointment, but never lose infinite hope

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,17 +1,12 @@
 #include "main.h"
 
-long long int main(int argc, char *argv[])
+int main(int argc, char __attribute__((unused))*argv[])
 {
 	int result = 0;
 
 	if (argc != 3)
 	{
-		_putchar('E');
-		_putchar('r');
-		_putchar('r');
-		_putchar('o');
-		_putchar('r');
-		_putchar('\n');
+		printf("Error\n");
 		exit(98);
 	}
 
