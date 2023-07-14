d1ffdf6c49f27cb3342da2a28f3f6117122ffed1
Christian Nwachukwu <programmerchris6002@gmail.com>
2023-07-13 21:32:01 -0700

    _calloc

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -9,18 +9,14 @@
 
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	unsigned int i;
 	int *array;
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
 
-	array = malloc(sizeof(int) * size);
+	array = malloc(nmemb * size);
 	if (array == NULL)
 		return (NULL);
-
-	for (i = 0; i < size; i++)
-		array[i] = nmemb;
 
 	return (array);
 }
