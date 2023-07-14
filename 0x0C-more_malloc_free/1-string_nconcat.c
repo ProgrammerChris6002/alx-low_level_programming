4b4f0ee478fe1ab5160f94c712e69d06a88581c2
Christian Nwachukwu <programmerchris6002@gmail.com>
2023-07-13 21:02:09 -0700

    string_nconcat

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -10,10 +10,31 @@
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
+	char *array;
+	int i, len1 = 0, len2 = 0;
+
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
 
-	return (s1)
+	while (s1[len1])
+		len1++;
+	while (s2[len2] && n > 0)
+	{
+		len2++;
+		n--;
+	}
+
+	array = malloc(sizeof(char) * len1);
+	if (array == NULL)
+		return (NULL);
+
+	for (i = 0; i < len1; i++)
+		array[i] = s1[i];
+	for (i = 0; i < len2; i++)
+		array[i + len1] = s2[i];
+	array[len1 + len2] = '\0';
+
+	return (array);
 }
