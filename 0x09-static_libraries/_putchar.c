3564c13d37462c7f671638c04114aebcfa3d967f
Christian Nwachukwu <programmerchris6002@gmail.com>
2023-07-28 10:27:58 -0700

    A library is not a luxury but one of the necessities of life

--- 0x09-static_libraries/_putchar.c
+++ 0x09-static_libraries/_putchar.c
@@ -1,1 +1,13 @@
 #include "main.h"
+
+/**
+  * A library is not a luxury but one of the necessities of life
+  * @c: The character to print
+  * Return: On success 1
+  * On error, -1 is returned, and errno is set appropriately
+  */
+
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
--- 0x09-static_libraries/main.h
+++ 0x09-static_libraries/main.h
@@ -2,6 +2,7 @@
 #define MAIN_H
 
 #include <stdio.h>
+#include <unistd.h>
 
 int _putchar(char c);
 int _islower(int c);
