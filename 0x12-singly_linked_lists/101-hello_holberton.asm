872962274b0e4e61d870a49b5a1cb00f202d24d7
Christian Nwachukwu <programmerchris6002@gmail.com>
2023-07-28 02:37:08 -0700

    Print list

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,6 +1,5 @@
 section .data
     hello db 'Hello, Holberton', 0  ; Null-terminated string
-    format db '%s', 0              ; Format string for printf
 
 section .text
     extern printf                 ; External reference to the C library function
@@ -8,13 +7,9 @@ section .text
     global main                   ; Entry point for the program
 
 main:
-    push rbp                      ; Save base pointer
-    mov rdi, format               ; Set rdi to the address of the format string
-    mov rsi, hello                ; Set rsi to the address of the hello string
+    mov rdi, hello                ; Set rdi to the address of the format string
     call printf                   ; Call printf function
-    add rsp, 8                    ; Clean up the stack after the printf call
 
     xor rax, rax                  ; Set return value to 0 (success)
-    pop rbp                       ; Restore base pointer
     ret                           ; Return from the program
 
