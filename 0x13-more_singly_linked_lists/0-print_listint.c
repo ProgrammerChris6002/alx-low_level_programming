fe1ed189e2ae5e92f283b36c6cd0e352837c0d0f
Christian Nwachukwu <programmerchris6002@gmail.com>
2023-07-31 22:52:01 -0700

    Print List

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,5 +1,11 @@
 #include "lists.h"
 
+/**
+  * print_listint - prints all the elements of a listint_t list
+  * @h: head node
+  * Return: Number of nodes
+  */
+
 size_t print_listint(const listint_t *h)
 {
 	int count = 0;
