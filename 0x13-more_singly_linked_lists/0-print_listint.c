49821e84725c5dd8f3440611fefcc9fcfb2f516e
Christian Nwachukwu <programmerchris6002@gmail.com>
2023-07-31 22:43:05 -0700

    Print List

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+size_t print_listint(const listint_t *h)
+{
+	int count = 0;
+
+	if (h == NULL)
+		return (-1);
+
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		count++;
+		h = h->next;
+	}
+
+	return (count);
+}
