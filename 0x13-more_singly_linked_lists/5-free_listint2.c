c489afd666b3e0ec824b88170b2f2bfc77c42ff6
Christian Nwachukwu <programmerchris6002@gmail.com>
2023-08-01 12:31:04 -0700

    Free

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -9,10 +9,15 @@ void free_listint2(listint_t **head)
 {
 	listint_t *current;
 
+	if (head == NULL)
+		return;
+
 	while (*head != NULL)
 	{
 		current = *head;
 		*head = (*head)->next;
 		free(current);
 	}
+
+	*head = NULL;
 }
