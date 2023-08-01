27d4e2059ce08f44b80cb73376147c97365a3634
Christian Nwachukwu <programmerchris6002@gmail.com>
2023-08-01 13:08:35 -0700

    Pop

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -11,12 +11,15 @@
 int pop_listint(listint_t **head)
 {
 	int n;
+	listint_t *temp;
 
 	if (*head == NULL)
 		return (0);
 
 	n = (*head)->n;
-	free(*head);
+	temp = *head;
+	*head = (*head)->next;
+	free(temp);
 
 	return (n);
 }
