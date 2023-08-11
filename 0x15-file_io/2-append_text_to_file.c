20d5990dd1feb7b746885e326d0ab5cc28518426
Christian Nwachukwu <programmerchris6002@gmail.com>
2023-08-11 13:53:42 -0700

    Speak gently, she can hear

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -11,33 +11,30 @@
 int append_text_to_file(const char *filename, char *text_content)
 {
 	int fd, bytes_written = 0;
+	ssize_t text_len = 0;
 
 	if (filename == NULL)
 		return (-1);
 
-	fd = open(filename, O_WRONLY | O_APPEND);
-	if (fd == -1)
-		return (-1);
-
 	if (text_content != NULL)
 	{
-		while (text_content[bytes_written])
-			bytes_written++;
+		while (text_content[text_len])
+			text_len++;
 
-		bytes_written = write(fd, text_content, bytes_written);
-		if (bytes_written == -1)
-		{
-			close(fd);
+		fd = open(filename, O_WRONLY | O_APPEND);
+		if (fd == -1)
 			return (-1);
-		}
+
+		bytes_written = write(fd, text_content, text_len);
+		close(fd);
+
+		if (bytes_written != text_len)
+			return (-1);
 	}
 	else
 	{
-		close(fd);
 		return (-1);
 	}
-
-	close(fd);
 
 	return (1);
 }
